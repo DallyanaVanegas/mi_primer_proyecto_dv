62b6dbf86fb6d12e08468193f3c98e6785b96c2f
Dallyana Vanegas <davanegas@uce.edu.ec>
2022-06-08 22:01:44 -0500

    Tarea6

--- js/misprimerosScripts.js
+++ js/misprimerosScripts.js
@@ -1,14 +1,17 @@
 function ocultarMostrarImagen() {
     //display: none, block
     var valorDisplay = document.getElementById("idImagen").style.display;
-    if (valorDisplay == 'none') {
+    console.log(valorDisplay)
+    if (valorDisplay== 'none') {
         valorDisplay = 'block';
+        console.log(valorDisplay)
         console.log('V')
     } else {
         valorDisplay = 'none';
+        console.log(valorDisplay)
         console.log('F')
     }
-    document.getElementById("idImagen").className = valorDisplay;
+    document.getElementById("idImagen").style.display = valorDisplay;
 }
 
 function enciende(){
@@ -19,4 +22,20 @@ function enciende(){
 function apaga(){
     var valorSRC = document.getElementById("imgFoco").src;
     document.getElementById("imgFoco").src='../img/apagado.png';
+}
+
+function encenderApagar() {
+    //display: none, block
+    var valorDisplay = document.getElementById("imgFoco").src;
+    console.log(valorDisplay)
+    if (valorDisplay.match('../img/encendido.png')) {
+        valorDisplay = '../img/apagado.png';
+        console.log(valorDisplay)
+        console.log('V')
+    } else {
+        valorDisplay = '../img/encendido.png';
+        console.log(valorDisplay)
+        console.log('F')
+    }
+    document.getElementById("imgFoco").src = valorDisplay;
 }
